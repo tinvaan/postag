d2dfb9554f9577a3bf4abea220d5c99b4514c914
Harish Navnit <harishnavnit@gmail.com>
2023-04-16 21:25:29 +0530

    Add source module

---
+++ postag/lambda_function.py
@@ -0,0 +1,22 @@
+
+import boto3
+import os
+
+from . import script
+
+s3 = boto3.resource('s3')
+
+
+def lambda_handler(event, context):
+    written = []
+    src = s3.Bucket(os.getenv('AWS_SOURCE_BUCKET'))
+    dst = s3.Bucket(os.getenv('AWS_DESTINATION_BUCKET'))
+
+    for obj in src.objects.all():
+        srcUri = f's3://{src.name}/{obj.key}'
+        dstUri = f's3://{dst.name}/{obj.key}'
+
+        script.write(dstUri, list(script.parse(srcUri)))
+        written.append(dstUri)
+
+    return {'status': 'updated', 'modified': written}
---
+++ postag/script.py
@@ -0,0 +1,37 @@
+
+import boto3
+import predict
+import requests
+
+from smart_open import open
+
+from libs.simpleais import sentences_from_source
+
+
+session = boto3.Session()
+
+
+class TLE:
+    session = requests.Session()
+    host = 'http://tle.spire.com'
+    qth = (37.771034, 122.413815, 7)
+
+    @classmethod
+    def get(cls, norad_id):
+        r = cls.session.get(cls.host + norad_id)
+        if not r.ok:
+            r.raise_for_status()
+        return r.text
+
+
+def parse(uri):
+    for sentence in sentences_from_source(uri):
+        if sentence.source and sentence.source not in ['terrestrial', 'dynamic']:
+            tle = TLE.get(int(sentence.source))
+            sat = predict.observe(tle, TLE.qth, sentence.time)
+            yield f'\p:{sat.get("latitude", "")},\q:{sat.get("longitude", "")},{sentence.string}'
+
+
+def write(uri, sentences):
+    with open(uri, 'wb', transport_params={'client': session.client('s3')}) as fout:
+        return fout.writelines(sentences)
