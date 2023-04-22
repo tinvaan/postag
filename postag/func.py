ed4fa3afa728ba3b02b2348fa8ce6abd92a93a89
Harish Navnit <harishnavnit@gmail.com>
2023-04-22 20:40:05 +0530

    Add object metadata for processed files

--- postag/func.py
+++ postag/func.py
@@ -2,6 +2,8 @@
 import boto3
 import os
 
+from datetime import datetime
+
 from postag import script
 
 
@@ -14,10 +16,11 @@ def run(event, context):
     dst = s3.Bucket(os.getenv('AWS_DESTINATION_BUCKET'))
 
     for obj in src.objects.all():
-        srcUri = f's3://{src.name}/{obj.key}'
-        dstUri = f's3://{dst.name}/{obj.key}'
-
-        script.write(dstUri, list(script.parse(srcUri)))
-        written.append(dstUri)
+        obj = obj.Object()
+        if not obj.metadata.get('processed'):
+            lines = script.parse(f's3://{src.name}/{obj.key}')
+            script.write(f's3://{dst.name}/{obj.key}', lines)
+            written.append(f's3://{dst.name}/{obj.key}')
+        obj.put(Metadata={'processed': datetime.now().isoformat()})
 
     return {'status': 'updated', 'modified': written}
--- tests/test_func.py
+++ tests/test_func.py
@@ -1,8 +1,10 @@
 
 import boto3
 import os
+import tempfile
 
 from moto import mock_s3
+from datetime import datetime
 
 from . import AWSEnv, stream
 
@@ -24,6 +26,18 @@ class TestLambdaHandler(AWSEnv):
 
         self.s3.create_bucket(Bucket=os.getenv('AWS_DESTINATION_BUCKET'))
 
+    def populate(self, bucket):
+        meta = {'processed': datetime.now().isoformat()}
+        for idx in range(20):
+            key = str(idx) + '.txt'
+            tmp = tempfile.mktemp(key)
+            with open(tmp, 'w') as f:
+                f.writelines(stream)
+
+            self.s3.upload_file(tmp, bucket, key)
+            if idx >= 5:
+                self.s3.put_object(Bucket=bucket, Key=key, Metadata=meta)
+
     def test_lambda_handler(self):
         # Pesky import
         # https://docs.getmoto.org/en/latest/docs/getting_started.html#what-about-those-pesky-imports
@@ -33,10 +47,22 @@ class TestLambdaHandler(AWSEnv):
         self.assertEqual(response.get('status'), 'updated')
         self.assertGreater(len(response.get('modified')), 0)
 
+    def test_unprocessed_writes(self):
+        # Pesky import
+        # https://docs.getmoto.org/en/latest/docs/getting_started.html#what-about-those-pesky-imports
+        from postag import func
+
+        self.populate(os.getenv('AWS_SOURCE_BUCKET'))
+        response = func.run(dict(), dict())
+        self.assertEqual(response.get('status'), 'updated')
+        self.assertEqual(len(response.get('modified')), 5 + 1)
+
     def tearDown(self):
         os.remove(self.tmp)
-        self.s3.delete_object(Bucket=os.getenv('AWS_SOURCE_BUCKET'), Key=self.key)
-        self.s3.delete_bucket(Bucket=os.getenv('AWS_SOURCE_BUCKET'))
+        keys = [self.key] + [str(idx) + '.txt' for idx in range(20)]
+        for key in keys:
+            self.s3.delete_object(Bucket=os.getenv('AWS_SOURCE_BUCKET'), Key=key)
+            self.s3.delete_object(Bucket=os.getenv('AWS_DESTINATION_BUCKET'), Key=key)
 
-        self.s3.delete_object(Bucket=os.getenv('AWS_DESTINATION_BUCKET'), Key=self.key)
+        self.s3.delete_bucket(Bucket=os.getenv('AWS_SOURCE_BUCKET'))
         self.s3.delete_bucket(Bucket=os.getenv('AWS_DESTINATION_BUCKET'))
